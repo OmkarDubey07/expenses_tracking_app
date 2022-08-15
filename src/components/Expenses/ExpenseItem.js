d58f4675274b2f768284eacb368d9eb11a7bd7be
Omkar nath dubey <omkardubey1530@gmail.com>
2022-08-15 16:16:41 +0530

    added input box to change the value

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,13 +6,29 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+    const [ newTitle , setNewTitle ] = useState("");
+
+
+    const [ title , setTitle ] = useState(props.title);
+
+    const clickHandler = () =>{
+        setTitle (newTitle);
+    }
+
+    const changeHandler = (event) =>{
+        setNewTitle(event.target.value);
+    }
+
     return (
         <Card className="expense-item" >
             <ExpenseDate date={props.date} />
             <div className="expense-item_description">
-                <h2> {props.title} </h2>
+                <h2> {title} </h2>
                 <div className="expense-item_price" > ${ props.amount} </div>
             </div>
+            <input type='text' value= {newTitle} onChange={ changeHandler }/>
+            <button onClick={clickHandler}>change title</button>
+
         </Card>
     );
 }
