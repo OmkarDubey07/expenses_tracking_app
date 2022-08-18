6678b7bff5fcb67d774a3074be5a2142408c9f26
Omkar nath dubey <omkardubey1530@gmail.com>
2022-08-18 14:16:50 +0530

    added form's data to object

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -18,22 +18,37 @@
         setEnteredDate(event.target.value)
 
     };
+    const submitHandler = (event) => {
+        event.preventDefault();  // To stop reloading when click on submit button.
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date : new Date (enteredDate)
+        }
+        console.log(expenseData);  
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+    };
 
     return( 
 
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min=" 0.01" step="1" onChange={amountChangeHandler} />
+                    <input type="number" value={enteredAmount} min=" 0.01" step="1" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" onChange={dateChangeHandler} />
+                    <input type="date" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
 
             </div>
