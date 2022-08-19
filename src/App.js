7fd96447354ba46e43979b99ebc5869e67174dbd
Omkar nath dubey <omkardubey1530@gmail.com>
2022-08-19 13:59:57 +0530

    now you can add wishlist

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpenses/NewExpense';
 
-const App = () => {
-    // let expenseDate = new Date(2022,6,18);
-    // let expenseTitle = "School fee";
-    // let expenseAmount = 300;
-    let expenses =[
-        {
-            id : 'e1',
-            title: 'School Fee',
-            amount: 250,
-            date: new Date(2022,6,18)
-        },
-        {
-            id : 'e2',
-            title: 'Mobile bill',
-            amount: 2500,
-            date: new Date(2022,5,18)
-        },
-        {
-            id : 'e3',
-            title: 'Electric bill',
-            amount: 3500,
-            date: new Date(2022,7,18)
-        },
-        {
-            id : 'e4',
-            title: 'Books',
-            amount: 200,
-            date: new Date(2022,4,18)
-        }
-    ];
 
-    const addExpenseHandler = (expense)=>{
-        console.log(expense);
+let DUMMY_EXPENSE = [
+    {
+        id: 'e1',
+        title: 'School Fee',
+        amount: 250,
+        date: new Date(2022, 6, 18)
+    },
+    {
+        id: 'e2',
+        title: 'Mobile bill',
+        amount: 2500,
+        date: new Date(2022, 5, 18)
+    },
+    {
+        id: 'e3',
+        title: 'Electric bill',
+        amount: 3500,
+        date: new Date(2022, 7, 18)
+    },
+    {
+        id: 'e4',
+        title: 'Books',
+        amount: 200,
+        date: new Date(2022, 4, 18)
+    }
+];
+
+const App = () => {
+
+    const [expenses , setExpenses] = useState(DUMMY_EXPENSE);
+
+    const addExpenseHandler = (expense) => {
+        const updatedExpense = [expense , ...expenses];
+        setExpenses(updatedExpense);
     };
 
 
-    return(
+    return (
         <div>
             <h2>Expense Tracking</h2>
-            <NewExpense onAddExpense = { addExpenseHandler} />
-            <Expenses item = { expenses } />
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses item={expenses} />
 
         </div>
     );
